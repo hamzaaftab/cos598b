b2b838dd38233387df3c302b791b64b8b383a277
hamzaaftab <hamzaaftabmalik@gmail.com>
2012-04-17 23:08:34 -0400

    server warning fix

--- appengine/data.py
+++ appengine/data.py
@@ -1,6 +1,5 @@
 from google.appengine.dist import use_library
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 use_library('django', '0.96')
 from google.appengine.ext import db
 from google.appengine.ext import webapp
