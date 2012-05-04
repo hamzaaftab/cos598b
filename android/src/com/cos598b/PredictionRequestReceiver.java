ebd926accf6607c219fa9304816bb9af694f0ce5
hamzaaftab <hamzaaftabmalik@gmail.com>
2012-05-03 20:52:35 -0400

    show toast when wifi expected soon

--- android/src/com/cos598b/PredictionRequestReceiver.java
+++ android/src/com/cos598b/PredictionRequestReceiver.java
@@ -95,6 +95,9 @@ public class PredictionRequestReceiver extends BroadcastReceiver {
         // if prediction is too long, then callback
         if (time_to_wifi > tolerance - timer) {
             callback("wifi will not be available soon");
+        } else {
+            // display prediction
+            Utils.toast(mContext, "Wifi unavailable, but expected in " + Integer.toString(time_to_wifi-timer) + " seconds");
         }
     }
 
